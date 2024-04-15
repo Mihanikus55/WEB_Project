ccc766334f6b0cfab241e2c512c5f4e81bb974f4
Mihanikus <mihaelnovikov2@yandex.ru>
2024-04-15 16:35:47 +0300

    Добавил базу данных с таблицей пользователей. Добавление данных пока не реализовано

--- main.py
+++ main.py
@@ -1,13 +1,51 @@
 import os
 
-from forms.user import LoginForm
+from data import db_session
+from data.users import User
+from forms.user import LoginForm, RegisterForm
 from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
-# @app.route("/", methods=['GET', 'POST']) # временно
+def main():
+    db_session.global_init("db/krypto.db")
+
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
+
+@app.route("/")
+def index():
+    return ""
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    # if form.validate_on_submit():
+    #     if form.password.data != form.password_again.data:
+    #         return render_template('register.html', title='Регистрация',
+    #                                form=form,
+    #                                message="Пароли не совпадают")
+    #     db_sess = db_session.create_session()
+    #     if db_sess.query(User).filter(User.email == form.email.data).first():
+    #         return render_template('register.html', title='Регистрация',
+    #                                form=form,
+    #                                message="Такой пользователь уже есть")
+    #     user = User(
+    #         name=form.name.data,
+    #         email=form.email.data,
+    #         about=form.about.data
+    #     )
+    #     user.set_password(form.password.data)
+    #     db_sess.add(user)
+    #     db_sess.commit()
+    #     return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
+
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
@@ -24,5 +62,4 @@ def login():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    main()
