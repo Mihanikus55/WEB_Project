c0f80ce4f0c089e8d0232130fe812855a51b36a5
PC <vbabynin@yandex.ru>
2024-04-21 17:19:45 +0300

    Добавление главной сраницы

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import os
 
-from flask_login import login_user, LoginManager, login_required, logout_user, current_user
-
 from data import db_session
 from data.users import User
 from forms.user import LoginForm, RegisterForm
@@ -9,9 +7,6 @@ from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
-
-login_manager = LoginManager()
-login_manager.init_app(app)
 
 
 def main():
@@ -23,61 +18,52 @@ def main():
 
 @app.route("/")
 def index():
-    if current_user.is_authenticated:
-        return f"<h3>Поздравляем, вы залогинились</h3>{current_user}"
-    return redirect("/login")
+    return ""
 
 
 @app.route('/register', methods=['GET', 'POST'])
-def register():
+def reqister():
     form = RegisterForm()
-    if form.validate_on_submit():
-        if form.password.data != form.password_again.data:
-            return render_template('register.html', title='Registration',
-                                   form=form,
-                                   message="Password mismatch")
-        db_sess = db_session.create_session()
-        if db_sess.query(User).filter(User.email == form.email.data).first():
-            return render_template('register.html', title='Registration',
-                                   form=form,
-                                   message="There is already such a user")
-        user = User(
-            email=form.email.data
-        )
-        user.set_password(form.password.data)
-        db_sess.add(user)
-        db_sess.commit()
-        login_user(user)
-        return redirect('/')
+    # if form.validate_on_submit():
+    #     if form.password.data != form.password_again.data:
+    #         return render_template('register.html', title='Регистрация',
+    #                                form=form,
+    #                                message="Пароли не совпадают")
+    #     db_sess = db_session.create_session()
+    #     if db_sess.query(User).filter(User.email == form.email.data).first():
+    #         return render_template('register.html', title='Регистрация',
+    #                                form=form,
+    #                                message="Такой пользователь уже есть")
+    #     user = User(
+    #         name=form.name.data,
+    #         email=form.email.data,
+    #         about=form.about.data
+    #     )
+    #     user.set_password(form.password.data)
+    #     db_sess.add(user)
+    #     db_sess.commit()
+    #     return redirect('/login')
     return render_template('register.html', title='Registration', form=form)
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
-    if form.validate_on_submit():
-        db_sess = db_session.create_session()
-        user = db_sess.query(User).filter(User.email == form.email.data).first()
-        if user and user.check_password(form.password.data):
-            login_user(user)
-            return redirect("/")
-        return render_template('login.html', title="Authorization",
-                               message="Incorrect login or password",
-                               form=form)
+    # if form.validate_on_submit():
+    #     db_sess = db_session.create_session()
+    #     user = db_sess.query(User).filter(User.email == form.email.data).first()
+    #     if user and user.check_password(form.password.data):
+    #         login_user(user, remember=form.remember_me.data)
+    #         return redirect("/")
+    #     return render_template('login.html',
+    #                            message="Неправильный логин или пароль",
+    #                            form=form)
     return render_template('login.html', title="Authorization", form=form)
 
 
-@login_manager.user_loader
-def load_user(user_id):
-    db_sess = db_session.create_session()
-    return db_sess.query(User).get(user_id)
-
-
-@app.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    return redirect("/register")
+@app.route('/lobby', methods=['GET', 'POST'])
+def lobby():
+    return render_template('main_page.html', title="MiVa")
 
 
 if __name__ == '__main__':
